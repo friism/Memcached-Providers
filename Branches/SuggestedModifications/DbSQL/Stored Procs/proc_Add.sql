5f1569592ea859cc43143cacd0386df54ae2827c
SND\warlock6x3_cp <SND\warlock6x3_cp@0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5>
2009-10-17 16:27:22 +0000

    I want to make some minor modifications pre-MySQL addition

---
+++ Branches/SuggestedModifications/DbSQL/Stored Procs/proc_Add.sql
@@ -0,0 +1,39 @@
+﻿IF Object_ID('proc_Add') IS NOT NULL	
+	Drop procedure [proc_Add]
+
+GO
+
+CREATE procedure [proc_Add]
+( 
+	@SessionId nvarchar(80),
+	@ApplicationName nvarchar(255),
+	@Created DateTime,
+	@Expires DateTime,
+	@LockDate Datetime,
+	@LockId int,
+	@Timeout int,
+	@Locked bit,
+	@SessionItems varbinary(max) = NULL,
+	@Flags int
+) 
+AS 
+	Begin Try
+		-- Deleting any already expired data
+		Delete from tblSessions where 
+			SessionId = @SessionId and
+			ApplicationName = @ApplicationName and
+			Expires < GetDate();
+
+		-- Inserting new session data
+		Insert Into tblSessions(
+			SessionId, ApplicationName, Created, Expires, LockDate, LockId, [Timeout],
+			Locked, SessionItems, Flags)
+		Values(@SessionId, @ApplicationName, @Created, @Expires, @LockDate, @LockId, @Timeout, 
+			@Locked, @SessionItems, @Flags);
+		return 1;
+	End Try
+	Begin Catch
+		return -1;
+	End Catch
+			
+GO
---
+++ Branches/SuggestedModifications/DbSQL/Stored Procs/proc_LockItemWithoutLockId.sql
@@ -0,0 +1,29 @@
+﻿IF EXISTS (SELECT * FROM sysobjects WHERE type = 'P' AND name = 'proc_LockItemWithoutLockId')
+	BEGIN
+		DROP  Procedure  proc_LockItemWithoutLockId
+	END
+
+GO
+
+CREATE Procedure proc_LockItemWithoutLockId
+(
+	@SessionId nvarchar(80),
+	@ApplicationName nvarchar(255)	
+)
+
+
+AS
+	
+	UPDATE tblSessions SET
+            Locked = 1, LockDate = GetDate()
+            WHERE SessionId = @SessionId AND ApplicationName = @ApplicationName
+            AND Locked = 0 AND Expires > GetDate();
+
+GO
+
+
+GRANT EXEC ON proc_LockItemWithoutLockId TO PUBLIC
+
+GO
+
+
