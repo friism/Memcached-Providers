a6f871982ceed76ff8241a45f5ffad00e3dac592
Michael Friis <friism@gmail.com>
2011-08-01 17:44:44 -0700

    remove non-trunk stuff

--- Branches/SuggestedModifications/DatabaseScripts/MySql/Stored Procs/proc_CleanExpiredData.sql
+++
@@ -1,8 +0,0 @@
-DROP PROCEDURE IF EXISTS proc_CleanExpiredData;
-
-CREATE PROCEDURE proc_CleanExpiredData ()
-BEGIN
-
-    DELETE FROM tblSessions WHERE Expires < sysDate();
-
-END
--- Branches/SuggestedModifications/DatabaseScripts/MySql/Stored Procs/proc_RemoveItem.sql
+++
@@ -1,9 +0,0 @@
-DROP PROCEDURE IF EXISTS proc_RemoveItem;
-
-CREATE PROCEDURE proc_RemoveItem(_SessionId          varchar(80),
-                                 _ApplicationName    varchar(255))
-   BEGIN
-      DELETE FROM tblSessions
-            WHERE SessionId = _SessionId
-                  AND ApplicationName = _ApplicationName;
-   END
